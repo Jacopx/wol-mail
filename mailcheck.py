90da7865f6ade9ab339565b0825a5e737648da70
Jacopx <jacopo.nasi@gmail.com>
2017-02-14 22:35:26 +0100

    New login function and time printing

--- mailcheck.py
+++ mailcheck.py
@@ -1,32 +1,36 @@
 # Importing all Dependencies
 import poplib
 from email import parser
-import datetime
 import time
 from wakeonlan import wol
 
+def login(f, user, pwd):
+    # Gmail POP3 Server
+    pop_link = poplib.POP3_SSL('pop.gmail.com')
+
+    # Try login
+    try:
+        pop_link.user(user)
+    except poplib.socket.error, poplib.error_proto:
+        f.write('LOGIN ERROR @ ' + time.strftime("%d-%m-%y %H:%M.%S") + '\n')
+    # Try password
+    try:
+        pop_link.pass_(pwd)
+    except poplib.socket.error, poplib.error_proto:
+        f.write('LOGIN ERROR @ ' + time.strftime("%d-%m-%y %H:%M.%S") + '\n')
+
+    return pop_link
+
 def main(f, MAC, user, pwd):
     # Writing starting time and divisor
-    f.write('Start: ' + str(datetime.datetime.now()) + '\n')
-    f.write('----------------------------------------------\n\n')
+    f.write('Start: ' + time.strftime("%d-%m-%y @ %H:%M.%S") + '\n')
+    f.write('----------------------------------------------\n')
 
+    t=10
     # Infinite cycle
     while 1:
-        # Saving time
-        isnow = datetime.datetime.now()
-        # Gmail POP3 Server
-        pop_link = poplib.POP3_SSL('pop.gmail.com')
-        # Try login
-        try:
-            pop_link.user(user)
-        except poplib.socket.error, poplib.error_proto:
-            f.write('LOGIN ERROR @ ' + str(datetime.datetime.now()) + '\n')
-        # Try password
-        try:
-            pop_link.pass_(pwd)
-        except poplib.socket.error, poplib.error_proto:
-            f.write('LOGIN ERROR @ ' + str(datetime.datetime.now()) + '\n')
-
+        # Call login function
+        pop_link=login(f, user, pwd)
         # Get messages from server:
         msg = [pop_link.retr(i) for i in range(1, len(pop_link.list()[1]) + 1)]
         # Concat message pieces:
@@ -35,38 +39,35 @@ def main(f, MAC, user, pwd):
 
         # Printing log and cheching MSG syntax
         for msg in msg:
-
             if msg['subject']=='WOL': # Correct Message receiver
-
-                f.write('WOL --> YES @ {0}:{1} from {2}\n'.format(isnow.hour, isnow.minute, msg['from']))
+                f.write('WOL --> YES @ {0} from {1}\n'.format(time.strftime("%H:%M.%S"), msg['from']))
                 # Sending MP to MAC, Broadcast IP and Port 9
                 wol.send_magic_packet(MAC, ip_address='255.255.255.255', port=9)
-
             else: # Wrong message receiver
+                f.write('WOL --> NO @ {0} from {1}\n'.format(time.strftime("%H:%M.%S"), msg['from']))
 
-                f.write('WOL --> NOT @ {0}:{1} from {2}\n'.format(isnow.hour, isnow.minute, msg['from']))
-
-        # Closing POP connection
+        # Close the active connection
         pop_link.quit()
+        # Waiting t time before recheck
+        time.sleep(t)
 
 # Managing signal KeyboardInterrupt
 if __name__ == '__main__':
 
     # Variables
     MAC = 'AA:BB:CC:DD:EE:FF'
-    user = 'example.example'
-    pwd = 'prova1234'
+    user = 'jacopo.nasi'
+    pwd = '123456prova'
 
     # Opening file with date of the day
-    isnow = datetime.datetime.now()
-    fname='WOL_' + str(isnow.month) + str(isnow.day) + '-' + str(isnow.hour) + str(isnow.minute) + '.log'
+    fname='WOL_' + time.strftime("%y%m%d-%H%M") + '.log'
     f = open(fname, 'w', 0)
 
     # Main call and KeyboardInterrupt handler
     try:
         main(f, MAC, user, pwd)
     except KeyboardInterrupt:
-        f.write('\n----------------------------------------------\n')
-        f.write('Stop: ' + str(datetime.datetime.now()))
+        f.write('----------------------------------------------\n')
+        f.write('Stop: ' + time.strftime("%d-%m-%y @ %H:%M.%S"))
     finally:
         f.close()
